7c1f589a9d42e65ab23f9ff206f17ac0d3f8841e
ticcat <serfgiorico@gmail.com>
2023-09-21 12:37:59 +0200

    Formatted ts files using prettier

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,14 +1,15 @@
 export default function Home() {
-  return (<>
-    <h1>Headline 1</h1>
-    <h2>Headline 2</h2>
-    <h3>Headline 3</h3>
-    <h4>Headline 4</h4>
-    <h5>Headline 5</h5>
-    <p className="subtitle sub1">Subtitle 1</p>
-    <p className="subtitle sub2">Subtitle 2</p>
-    <p className="body b1">Body 1</p>
-    <p className="body b2">Body 2</p>
-  </>
-  )
+  return (
+    <>
+      <h1>Headline 1</h1>
+      <h2>Headline 2</h2>
+      <h3>Headline 3</h3>
+      <h4>Headline 4</h4>
+      <h5>Headline 5</h5>
+      <p className="subtitle sub1">Subtitle 1</p>
+      <p className="subtitle sub2">Subtitle 2</p>
+      <p className="body b1">Body 1</p>
+      <p className="body b2">Body 2</p>
+    </>
+  );
 }
