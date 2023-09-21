0317c897cc8d322bb3edaeb7767edbeeea4e157f
ticcat <serfgiorico@gmail.com>
2023-09-21 13:40:23 +0200

    Added anchor to author reference

--- web/src/components/AuthorRefAnchor/AuthorRefAnchor.tsx
+++ web/src/components/AuthorRefAnchor/AuthorRefAnchor.tsx
@@ -2,8 +2,13 @@ import styles from "./AuthorRefAnchor.module.css";
 
 export default function AuthorRefAnchor() {
   return (
-    <p className={styles.text}>
+    <a
+      className={styles.text}
+      href="https://twitter.com/serrial3"
+      target="_blank"
+      rel="noreferrer"
+    >
       <span className={styles.heart}>❥</span> By Sergio
-    </p>
+    </a>
   );
 }
