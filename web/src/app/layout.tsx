939a76470ad6adcbc5d3e46ab5102e95346848d3
ticcat <serfgiorico@gmail.com>
2023-09-21 13:37:44 +0200

    Created layout with author reference

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,8 +1,6 @@
-import "./globals.css";
+import AuthorRefAnchor from "@/components/AuthorRefAnchor/AuthorRefAnchor";
+import "@/styles/globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +14,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <div className="root-layout">
+          <AuthorRefAnchor></AuthorRefAnchor>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
