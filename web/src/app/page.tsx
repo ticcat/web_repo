4ac9dbb4f6805c6cfb761aaef8ccc573b7f106de
ticcat <serfgiorico@gmail.com>
2023-09-22 09:37:25 +0200

    Moved header styling to globalclasses file

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,9 @@
+import styles from "./page.module.css";
+
 export default function Home() {
   return (
     <>
-      <h1>Web page</h1>
+      <h1 className={styles.h1}>Web page</h1>
     </>
   );
 }
