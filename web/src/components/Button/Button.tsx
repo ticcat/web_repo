7956b6305f3a369b5bf40f6196c2fb5817b7c7a1
ticcat <serfgiorico@gmail.com>
2023-09-25 13:13:38 +0200

    Added button hover animation

--- web/src/components/Button/Button.tsx
+++ web/src/components/Button/Button.tsx
@@ -1,5 +1,11 @@
 import styles from "./Button.module.css";
 
 export default function Button({ children }: { children: React.ReactNode }) {
-  return <div className={styles.container}>{children}</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.fill1}></div>
+      <div className={styles.fill2}></div>
+      <div className={styles.content}>{children}</div>
+    </div>
+  );
 }
