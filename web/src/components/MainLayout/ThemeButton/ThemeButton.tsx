5303a92aec0867d88db71f2d9b2b7da9cdea4a9a
ticcat <serfgiorico@gmail.com>
2023-09-26 14:10:28 +0200

    Fixed first color change on mount

--- web/src/components/MainLayout/ThemeButton/ThemeButton.tsx
+++ web/src/components/MainLayout/ThemeButton/ThemeButton.tsx
@@ -14,10 +14,14 @@ function detectColorScheme() {
   }
 }
 
+function changeTheme(newTheme: string) {
+  document.documentElement.setAttribute("color-theme", newTheme);
+}
+
 export default function ThemeButton() {
   const [currentTheme, setCurrentTheme] = useState(detectColorScheme());
 
-  const changeTheme = useCallback(
+  const changeThemeCallback = useCallback(
     (newTheme?: string) => {
       const theme = newTheme
         ? newTheme
@@ -25,7 +29,7 @@ export default function ThemeButton() {
         ? "dark"
         : "light";
 
-      document.documentElement.setAttribute("color-theme", theme);
+      changeTheme(theme);
       setCurrentTheme(theme);
     },
     [currentTheme]
@@ -35,19 +39,27 @@ export default function ThemeButton() {
     window
       .matchMedia("(prefers-color-scheme: dark)")
       .addEventListener("change", (event) => {
-        changeTheme(event.matches ? "dark" : "light");
+        changeThemeCallback(event.matches ? "dark" : "light");
       });
     return () => {
       window
         .matchMedia("(prefers-color-scheme: dark)")
         .removeEventListener("change", (event) => {
-          changeTheme(event.matches ? "dark" : "light");
+          changeThemeCallback(event.matches ? "dark" : "light");
         });
     };
-  });
+  }, [changeThemeCallback, currentTheme]);
+
+  useEffect(() => {
+    //Manual color theme setter to execute on first mount
+    changeTheme(detectColorScheme());
+  }, []);
 
   return (
-    <button className={styles.themeButton} onClick={() => changeTheme()}>
+    <button
+      className={styles.themeButton}
+      onClick={() => changeThemeCallback()}
+    >
       {currentTheme === "light" ? "Dark mode" : "Light mode"}
     </button>
   );
