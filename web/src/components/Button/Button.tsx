ac7a2fbd7bc763db3c83509a7534f62f930aa9ad
ticcat <serfgiorico@gmail.com>
2023-09-26 11:54:18 +0200

    Changed button cursor and added click handler prop

--- web/src/components/Button/Button.tsx
+++ web/src/components/Button/Button.tsx
@@ -1,8 +1,16 @@
+"use client";
+
 import styles from "./Button.module.css";
 
-export default function Button({ children }: { children: React.ReactNode }) {
+export default function Button({
+  children,
+  clickHandler,
+}: {
+  children: React.ReactNode;
+  clickHandler: () => void;
+}) {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={() => clickHandler()}>
       <div className={styles.fill1}></div>
       <div className={styles.fill2}></div>
       <div className={styles.content}>{children}</div>
