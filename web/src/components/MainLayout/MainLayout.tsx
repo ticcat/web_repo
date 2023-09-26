be3bf2cfd59d517f8e33439d0110529c13260263
ticcat <serfgiorico@gmail.com>
2023-09-26 11:55:32 +0200

    Refactored main layout to use new authro reference button

--- web/src/components/MainLayout/MainLayout.tsx
+++ web/src/components/MainLayout/MainLayout.tsx
@@ -1,18 +1,5 @@
 import styles from "./MainLayout.module.css";
-import Button from "@/components/Button/Button";
-
-function AuthorRefAnchor() {
-  return (
-    <a
-      className={styles.authorRefText}
-      href="https://twitter.com/serrial3"
-      target="_blank"
-      rel="noreferrer"
-    >
-      <span className={styles.heart}>❥</span> By Sergio
-    </a>
-  );
-}
+import AuthorRefButton from "./AuthorRefButton/AuthorRefButton";
 
 export default function MainLayout({
   children,
@@ -25,9 +12,7 @@ export default function MainLayout({
       <div className={styles.content}>{children}</div>
       <div className={styles.botLine}></div>
       <div className={styles.authorRefButton}>
-        <Button>
-          <AuthorRefAnchor></AuthorRefAnchor>
-        </Button>
+        <AuthorRefButton></AuthorRefButton>
       </div>
     </div>
   );
