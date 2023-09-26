7c3c23ea73f055903eee3a3e95499e65ecdfa5a7
ticcat <serfgiorico@gmail.com>
2023-09-26 11:54:59 +0200

    Created new author reference button component

---
+++ web/src/components/MainLayout/AuthorRefButton/AuthorRefButton.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import styles from "./AuthorRefButton.module.css";
+import Button from "@/components/Button/Button";
+
+export default function AuthorRefButton() {
+  return (
+    <Button clickHandler={() => window.open("https://twitter.com/serrial3")}>
+      <div className={styles.authorRefButton}>
+        <div className={styles.authorRefText}>
+          <span className={styles.heart}>❥</span> By Sergio
+        </div>
+      </div>
+    </Button>
+  );
+}
