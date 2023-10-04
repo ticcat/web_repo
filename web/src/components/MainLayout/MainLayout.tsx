620a80c835c97a728f4a618962279d7124d63d99
ticcat <serfgiorico@gmail.com>
2023-10-04 11:39:19 +0200

    Added loading screen an test LinkButtons

--- web/src/components/MainLayout/MainLayout.tsx
+++ web/src/components/MainLayout/MainLayout.tsx
@@ -1,18 +1,37 @@
+"use client";
+
 import styles from "./MainLayout.module.css";
 import AuthorRefButton from "./AuthorRefButton/AuthorRefButton";
 import ThemeToggle from "./ThemeToggle/ThemeButton";
+import LoadingScreen from "../LoadingScreen/LoadingScreen";
+import LinkButton from "../Buttons/LinkButton/LinkButton";
+import { useState } from "react";
 
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [loading, setLoading] = useState(false);
+  const [nextPage, setNextPage] = useState("");
+
   return (
     <div className={styles.container}>
+      <LoadingScreen loading={loading} title={nextPage}></LoadingScreen>
       <div className={styles.topLine}></div>
-      <div className={styles.content}>{children}</div>
+      <main className={styles.content}>{children}</main>
       <div className={styles.botLine}></div>
       <div className={styles.authorRefButton}>
+        <LinkButton
+          href="/about"
+          setLoading={setLoading}
+          setNextPage={setNextPage}
+        >
+          About
+        </LinkButton>
+        <LinkButton href="/" setLoading={setLoading} setNextPage={setNextPage}>
+          Home
+        </LinkButton>
         <AuthorRefButton></AuthorRefButton>
         <ThemeToggle></ThemeToggle>
       </div>
