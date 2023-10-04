2640e4d91ebb3ce287a39c9b5d4962b58adbc4d8
ticcat <serfgiorico@gmail.com>
2023-10-04 12:55:45 +0200

    Hid test nav buttons

--- web/src/components/MainLayout/MainLayout.tsx
+++ web/src/components/MainLayout/MainLayout.tsx
@@ -1,41 +1,20 @@
-"use client";
-
 import styles from "./MainLayout.module.css";
 import AuthorRefButton from "./AuthorRefButton/AuthorRefButton";
 import ThemeToggle from "./ThemeToggle/ThemeButton";
 import LoadingScreen from "../LoadingScreen/LoadingScreen";
-import LinkButton from "../Buttons/LinkButton/LinkButton";
-import { useState } from "react";
 
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [loading, setLoading] = useState(false);
-  const [nextPageName, setNextPageName] = useState("");
-
   return (
     <div className={styles.container}>
-      <LoadingScreen loading={loading} title={nextPageName}></LoadingScreen>
+      <LoadingScreen loading={false} title={"welcome"}></LoadingScreen>
       <div className={styles.topLine}></div>
       <main className={styles.content}>{children}</main>
       <div className={styles.botLine}></div>
       <div className={styles.authorRefButton}>
-        <LinkButton
-          href="/about"
-          setLoading={setLoading}
-          setNextPageName={setNextPageName}
-        >
-          About
-        </LinkButton>
-        <LinkButton
-          href="/"
-          setLoading={setLoading}
-          setNextPageName={setNextPageName}
-        >
-          Home
-        </LinkButton>
         <AuthorRefButton></AuthorRefButton>
         <ThemeToggle></ThemeToggle>
       </div>
