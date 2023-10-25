2a3688eac7e3c971542be9ce1c51200f3eb536bf
ticcat <serfgiorico@gmail.com>
2023-10-25 12:51:21 +0200

    Added new variables to host short texts in studies and experience

--- web/src/components/StudiesWorkSections/StudiesWorkLanding/StudiesWorkLanding.tsx
+++ web/src/components/StudiesWorkSections/StudiesWorkLanding/StudiesWorkLanding.tsx
@@ -2,6 +2,25 @@ import AnimatedElement from "@/components/AnimatedElement/AnimatedElement";
 import styles from "./StudiesWorkLanding.module.css";
 
 export default function StudiesWorkLanding() {
+  const shortStudiesText = (
+    <>
+      {" "}
+      I have a bachelors degree in Computer Science at Universitat Politècnica
+      de València *UPV* and I have participated in an exchange program where I
+      attended a semester at Korea University *KU* in Seoul,South Korea.
+    </>
+  );
+  const shortExperienceText = (
+    <>
+      {" "}
+      After finishing my studies, I decided to enter the professional world as a
+      software developer. After two and half years of experience I decided to
+      pursue new challenges and opportunities by learning modern web
+      technologies widening my skillset and increasing my knowledge in front-end
+      development and design.
+    </>
+  );
+
   return (
     <div className={styles.container}>
       <div className={styles.title}>
@@ -10,17 +29,10 @@ export default function StudiesWorkLanding() {
       </div>
       <div className={styles.textContainer}>
         <div className={styles.text}>
-          I have a bachelors degree in Computer Science at Universitat
-          Politècnica de València *UPV* and I have participated in an exchange
-          program where I attended a semester at Korea University *KU* in Seoul,
-          South Korea.
+          <AnimatedElement showOnRender>{shortStudiesText}</AnimatedElement>
         </div>
         <div className={styles.text}>
-          After finishing my studies, I decided to enter the professional world
-          as a software developer. After two and half years of experience I
-          decided to pursue new challenges and opportunities by learning modern
-          web technologies widening my skillset and increasing my knowledge in
-          front-end development and design.
+          <AnimatedElement showOnRender>{shortExperienceText}</AnimatedElement>
         </div>
       </div>
     </div>
