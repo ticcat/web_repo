624a5f2d386327e4b2fd959885af17c8f5654746
ticcat <serfgiorico@gmail.com>
2023-10-26 12:39:17 +0200

    Used new animated element prop to animate correctly short texts

--- web/src/components/StudiesWorkSections/StudiesWorkLanding/StudiesWorkLanding.tsx
+++ web/src/components/StudiesWorkSections/StudiesWorkLanding/StudiesWorkLanding.tsx
@@ -5,24 +5,10 @@ import styles from "./StudiesWorkLanding.module.css";
 import Button from "@/components/Buttons/Button/Button";
 
 export default function StudiesWorkLanding() {
-  const shortStudiesText = (
-    <>
-      {" "}
-      I have a bachelors degree in Computer Science at Universitat Politècnica
-      de València *UPV* and I have participated in an exchange program where I
-      attended a semester at Korea University *KU* in Seoul,South Korea.
-    </>
-  );
-  const shortExperienceText = (
-    <>
-      {" "}
-      After finishing my studies, I decided to enter the professional world as a
-      software developer. After two and half years of experience I decided to
-      pursue new challenges and opportunities by learning modern web
-      technologies widening my skillset and increasing my knowledge in front-end
-      development and design.
-    </>
-  );
+  const shortStudiesText =
+    "I have a bachelors degree in Computer Science at Universitat Politècnica de València *UPV* and I have participated in an exchange program where I attended a semester at Korea University *KU* in Seoul, South Korea.";
+  const shortExperienceText =
+    "After finishing my studies, I decided to enter the professional world as a software developer. After two and half years of experience I decided to pursue new challenges and opportunities by learning modern web technologies widening my skillset and increasing my knowledge in front-end development and design.";
 
   return (
     <div className={styles.container}>
@@ -32,7 +18,12 @@ export default function StudiesWorkLanding() {
       </div>
       <div className={styles.textContainer}>
         <div className={styles.text}>
-          <AnimatedElement showOnRender>{shortStudiesText}</AnimatedElement>
+          <p>
+            <AnimatedElement
+              showOnRender
+              text={shortStudiesText}
+            ></AnimatedElement>
+          </p>
           <AnimatedElement showOnRender>
             <div className={styles.goToBtnContainer}>
               <Button clickHandler={() => {}}>
@@ -42,7 +33,12 @@ export default function StudiesWorkLanding() {
           </AnimatedElement>
         </div>
         <div className={styles.text}>
-          <AnimatedElement showOnRender>{shortExperienceText}</AnimatedElement>
+          <p>
+            <AnimatedElement
+              showOnRender
+              text={shortExperienceText}
+            ></AnimatedElement>
+          </p>
           <AnimatedElement showOnRender>
             <div className={styles.goToBtnContainer}>
               <Button clickHandler={() => {}}>
