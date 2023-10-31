02a5893d290f277ceada35953a05d0cc2cef04fa
ticcat <serfgiorico@gmail.com>
2023-10-31 12:35:01 +0100

    Added work section to studies&work page

--- web/src/app/studiesnwork/page.tsx
+++ web/src/app/studiesnwork/page.tsx
@@ -1,11 +1,13 @@
 import Studies from "@/components/StudiesWorkSections/Studies/Studies";
 import StudiesWorkLanding from "@/components/StudiesWorkSections/StudiesWorkLanding/StudiesWorkLanding";
+import Work from "@/components/StudiesWorkSections/Work/Work";
 
 export default function StudiesNwork() {
   return (
     <>
       <StudiesWorkLanding></StudiesWorkLanding>
       <Studies></Studies>
+      <Work></Work>
     </>
   );
 }
