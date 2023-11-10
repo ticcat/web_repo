25189290053db0f528bce241b359c2865f41df05
ticcat <serfgiorico@gmail.com>
2023-11-10 11:02:22 +0100

    Modified contact buttons style and html

--- web/src/components/Buttons/ContactButtons/ContactButtons.tsx
+++ web/src/components/Buttons/ContactButtons/ContactButtons.tsx
@@ -1,20 +1,23 @@
-import EmailButton from "@/components/Buttons/EmailButton/EmailButton";
-import SocialMediaButtons from "@/components/Buttons/SMNButtons/SMNButtons";
-import CVButton from "@/components/Buttons/CVButton/CVButton";
+import EmailButton from "@/components/Buttons/ContactButtons/EmailButton/EmailButton";
+import SocialMediaButtons from "@/components/Buttons/ContactButtons/SMNButtons/SMNButtons";
+import CVButton from "@/components/Buttons/ContactButtons/CVButton/CVButton";
 import styles from "./ContactButtons.module.css";
+import PhoneButton from "./PhoneButton/PhoneButton";
 
 export default function ContactButtons() {
   return (
-    <>
-      {" "}
+    <div className={styles.container}>
       <div className={styles.links}>
         <span className={styles.separator}>//</span>
         <EmailButton></EmailButton>
         <span className={styles.separator}>//</span>
         <CVButton></CVButton>
         <span className={styles.separator}>//</span>
+        <PhoneButton></PhoneButton>
       </div>
-      <SocialMediaButtons></SocialMediaButtons>
-    </>
+      <div className={styles.smnButtons}>
+        <SocialMediaButtons></SocialMediaButtons>
+      </div>
+    </div>
   );
 }
