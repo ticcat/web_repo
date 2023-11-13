54f62aecf26a7486bf2d056d0492bca68ec62fbb
ticcat <serfgiorico@gmail.com>
2023-11-13 12:41:36 +0100

    Fetching study data from server in studies page

--- web/src/components/StudiesWorkSections/Studies/Studies.tsx
+++ web/src/components/StudiesWorkSections/Studies/Studies.tsx
@@ -1,9 +1,40 @@
+"use client";
+
 import AnimatedElement from "@/components/AnimatedElement/AnimatedElement";
 import styles from "./Studies.module.css";
+import { useEffect, useState } from "react";
+import StudyEntryInfo from "@/dbClasses/StudyEntry";
+
+function StudyEntry({ entry }: { entry: StudyEntryInfo }) {
+  return (
+    <div className={styles.studyEntry}>
+      <div className={styles.entryHeader}>
+        <div className={styles.studyTitle}>
+          <AnimatedElement>{entry.title} //</AnimatedElement>
+          <br></br>
+          <AnimatedElement>{entry.duration}</AnimatedElement>
+        </div>
+        <div className={styles.studySubtitle}>
+          <AnimatedElement>{entry.subtitle}</AnimatedElement>
+        </div>
+      </div>
+      <div className={styles.entryText}>
+        <AnimatedElement text={entry.text}></AnimatedElement>
+      </div>
+    </div>
+  );
+}
 
 export default function Studies() {
-  const longLoremIpsum =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Et ultrices neque ornare aenean euismod elementum nisi. Urna cursus eget nunc scelerisque viverra mauris. Viverra ipsum nunc aliquet bibendum enim facilisis gravida neque. Turpis tincidunt id aliquet risus feugiat in ante. Feugiat in fermentum posuere urna. Pulvinar pellentesque habitant morbi tristique senectus et netus et malesuada. Urna id volutpat lacus laoreet. A pellentesque sit amet porttitor. Nibh nisl condimentum id venenatis a. Vestibulum lectus mauris ultrices eros in. Gravida cum sociis natoque penatibus et magnis dis parturient montes. Mauris pharetra et ultrices neque ornare. Malesuada bibendum arcu vitae elementum curabitur vitae. Vitae congue eu consequat ac felis. Lobortis mattis aliquam faucibus purus in.  ";
+  const [studyEntries, setStudyEntries] = useState<StudyEntryInfo[]>([]);
+
+  useEffect(() => {
+    fetch("/api/studies")
+      .then((res) => res?.json())
+      .then((data) => setStudyEntries(data));
+
+    return setStudyEntries([]);
+  }, []);
 
   return (
     <div id="studies" className={styles.container}>
@@ -11,45 +42,11 @@ export default function Studies() {
         <AnimatedElement>STUDIES</AnimatedElement>
       </div>
       <div className={styles.entries}>
-        <div className={styles.studyEntry}>
-          <div className={styles.entryHeader}>
-            <div className={styles.studyTitle}>
-              <AnimatedElement>
-                Universitat Politècnica de València //
-              </AnimatedElement>
-              <br></br>
-              <AnimatedElement>2016 - 2022</AnimatedElement>
-            </div>
-            <div className={styles.studySubtitle}>
-              <AnimatedElement>
-                Bachelors degree in computer science // Spec. Software
-                Engineering
-              </AnimatedElement>
-            </div>
-          </div>
-          <div className={styles.entryText}>
-            <AnimatedElement text={longLoremIpsum}></AnimatedElement>
-          </div>
-        </div>
-        <div className={styles.studyEntry}>
-          <div className={styles.entryHeader}>
-            <div className={styles.studyTitle}>
-              <AnimatedElement>
-                Korea University // 고려대학교 //
-              </AnimatedElement>
-              <br></br>
-              <AnimatedElement>Feb. 2020 - Aug. 2020</AnimatedElement>
-            </div>
-            <div className={styles.studySubtitle}>
-              <AnimatedElement>
-                English studies // Artificial Intelligence class
-              </AnimatedElement>
-            </div>
-          </div>
-          <div className={styles.entryText}>
-            <AnimatedElement text={longLoremIpsum}></AnimatedElement>
-          </div>
-        </div>
+        {studyEntries.map((entry) => {
+          return (
+            <StudyEntry key={entry.id.toString()} entry={entry}></StudyEntry>
+          );
+        })}
       </div>
     </div>
   );
