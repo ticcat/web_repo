3a266ad9c9c00ce33193515d8fca338fc6b1199a
ticcat <serfgiorico@gmail.com>
2023-11-15 11:36:38 +0100

    Added contact form, contact buttons and thanks text to contact page

--- web/src/components/ContactSections/ContactPage/ContactPage.tsx
+++ web/src/components/ContactSections/ContactPage/ContactPage.tsx
@@ -1,9 +1,17 @@
+import AnimatedElement from "@/components/AnimatedElement/AnimatedElement";
+import ContactForm from "./ContactForm/ContactForm";
 import styles from "./ContactPage.module.css";
+import ContactButtons from "@/components/Buttons/ContactButtons/ContactButtons";
 
 export default function ContactPage() {
   return (
     <div className={styles.container}>
-      <div className={styles.title}>CONTACT</div>
+      <AnimatedElement>
+        <div className={styles.title}>CONTACT</div>
+      </AnimatedElement>
+      <ContactForm></ContactForm>
+      <ContactButtons></ContactButtons>
+      <div className={styles.thanksText}>Thank you for dropping by!</div>
     </div>
   );
 }
