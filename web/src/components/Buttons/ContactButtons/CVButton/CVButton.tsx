092d10adf3b6d373eada9a79137a739cd597c914
ticcat <serfgiorico@gmail.com>
2023-12-05 13:06:32 +0100

    Added light and dark themed cvs depending on user theme

--- web/src/components/Buttons/ContactButtons/CVButton/CVButton.tsx
+++ web/src/components/Buttons/ContactButtons/CVButton/CVButton.tsx
@@ -3,19 +3,29 @@
 import Button from "../../Button/Button";
 import Link from "next/link";
 import styles from "./CVButton.module.css";
+import { getSetting } from "@/utils/userConfig";
+import { useState } from "react";
 
 export default function CVButton() {
+  const [CVFilename, setCVFilename] = useState("SergioRicoCVEng2Dark.pdf");
+
   return (
-    <Link
-      className={styles.link}
-      href="/files/SergioRicoCVEng.pdf"
-      target="_blank"
+    <Button
+      clickHandler={() => {
+        getSetting("theme", "dark") === "dark"
+          ? setCVFilename("SergioRicoCVEng2Dark.pdf")
+          : setCVFilename("SergioRicoCVEng2Light.pdf");
+      }}
     >
-      <Button clickHandler={() => {}}>
+      <Link
+        className={styles.link}
+        href={`/files/${CVFilename}`}
+        target="_blank"
+      >
         <div className={styles.text}>
           CV<span className={styles.arrow}>↓</span>
         </div>
-      </Button>
-    </Link>
+      </Link>
+    </Button>
   );
 }
